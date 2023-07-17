d55c4e49fc850447dd95f68ff226df5c77d0acb0
eterna8 <eterna8@proton.me>
2023-07-17 11:01:49 +0300

    a little bit updates

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
 echo welcome to the dotfiles installation
 echo if you get any error,report to my discord:_._eterna_._
 echo copying files to .config...
-cd config && cp -r alacritty  dunst  i3  polybar  rofi  $HOME/.config 
+cd config && cp -r alacritty  dunst  i3  polybar picom rofi zsh  $HOME/.config 
 cd -
 echo copying wallpapers to /pictures/backdrops...
 cp -r backdrops $HOME/pictures
+echo enabling kvantum...
+QT_QPA_PLATFORMTHEME=kvantum
+echo please restart your computer for changing theme.
 echo done!
