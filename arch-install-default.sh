6e3be756242ed99066153442bfb415f579184e23
eterna8 <eterna8@proton.me>
2023-07-21 23:05:35 +0300

    added default theme

---
+++ arch-install-default.sh
@@ -0,0 +1,6 @@
+echo downoad needed packages started...
+sudo pacman -S polybar i3 dolphin alacritty dunst rofi qt5ct kvantum  flameshot ttf-font-awesome ttf-cascadia-code zsh
+chsh -s /bin/zsh
+yay -S zsh-theme-powerlevel10k-git
+echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+sh default-setup.sh 
--- default-setup.sh
+++ default-setup.sh
@@ -1,7 +1,7 @@
 echo welcome to the dotfiles installation
 echo if you get any error,report to my discord:_._eterna_._
 echo copying files to .config...
-cd config && cp -r alacritty  dunst  i3  polybar picom rofi zsh  $HOME/.config 
+cd defaultc && cp -r alacritty  dunst  i3  polybar rofi zsh  $HOME/.config 
 cd -
 echo copying wallpapers to /pictures/backdrops...
 cp -r backdrops $HOME/pictures
@@ -9,5 +9,5 @@ echo enabling kvantum...
 QT_QPA_PLATFORMTHEME=kvantum
 echo please restart your computer for changing theme.
 echo installing neocat...
-cd Smile-NeoCat.sh &&
-echo done!
+cd Smile-NeoCat-Theme && bash ./smile-neocat.sh 
+echo done! 
