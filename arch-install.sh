61158827c003fc4617f5c25e051d7d3f446e23e7
eterna8 <eterna8@proton.me>
2023-07-26 13:27:05 +0300

    big update

--- arch-install.sh
+++ arch-install.sh
@@ -1,5 +1,6 @@
 echo downoad needed packages started...
-sudo pacman -S polybar i3 dolphin alacritty dunst rofi qt5ct kvantum  flameshot ttf-font-awesome picom ttf-cascadia-code zsh kcolorchooser
+sudo pacman -S polybar dolphin alacritty dunst rofi qt5ct kvantum  flameshot ttf-font-awesome picom ttf-cascadia-code zsh kcolorchooser
+yay -S i3-gaps-rounded-git
 chsh -s /bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 yay -S zsh-theme-powerlevel10k-git
