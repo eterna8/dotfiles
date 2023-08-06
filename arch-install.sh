c67af8a339af69a3edf425f8e09b0ace6f96d8fa
eterna8 <eterna8@proton.me>
2023-08-06 14:12:16 +0300

    polybar update

--- arch-install.sh
+++ arch-install.sh
@@ -1,8 +1,8 @@
 echo downoad needed packages started...
-sudo pacman -S polybar dolphin alacritty dunst rofi qt5ct kvantum  flameshot ttf-font-awesome picom ttf-cascadia-code zsh kcolorchooser
+sudo pacman -S polybar dolphin alacritty dunst rofi qt5ct kvantum  flameshot ttf-font-awesome ttf-cascadia-code zsh kcolorchooser
 yay -S i3-gaps-rounded-git
 chsh -s /bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 yay -S zsh-theme-powerlevel10k-git polybar-spotify-module
 echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
-sh setup.sh
+sh setup.sh 
--- transperent-setup.sh
+++ transperent-setup.sh
@@ -1,7 +1,7 @@
 echo welcome to the dotfiles installation
 echo if you get any error,report to my discord:_._eterna_._
 echo copying files to .config...
-cd defaultc && cp -r alacritty  dunst  i3  polybar rofi zsh  $HOME/.config 
+cd config && cp -r alacritty  dunst  i3  polybar picom rofi zsh  $HOME/.config 
 cd -
 echo copying wallpapers to /pictures/backdrops...
 cp -r backdrops $HOME/pictures
@@ -10,6 +10,9 @@ QT_QPA_PLATFORMTHEME=kvantum
 echo please restart your computer for changing theme.
 echo installing NeoCat...
 sleep 1
+cd NeoCat
 git clone --depth 1 https://github.com/m3tozz/NeoCat.git && cd NeoCat && sudo make install
-cd NeoCat && bash ./neocat.sh 
-echo done! 
+bash ./neocat.sh 
+sleep2
+clear
+echo done!
