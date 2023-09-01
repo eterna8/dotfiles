61267fc1679d1fbd1440b0e8f912eccd94a55220
eterna8 <eterna8@proton.me>
2023-09-01 22:03:36 +0300

    some fixes

--- arch-install.sh
+++ arch-install.sh
@@ -1,5 +1,5 @@
 echo downoad needed packages started...
-sudo pacman -S polybar dolphin alacritty dunst rofi qt5ct kvantum  flameshot ttf-font-awesome ttf-cascadia-code zsh kcolorchooser
+sudo pacman -S polybar dolphin alacritty dunst rofi qt5ct kvantum  flameshot ttf-font-awesome ttf-cascadia-code zsh kcolorchooser nitrogen
 git clone --depth 1 https://aur.archlinux.org/packages/i3-gaps-rounded-git && cd i3-gaps-rounded-git && makepkg -si && cd ..
 git clone --depth 1 https://aur.archlinux.org/packages/zsh-theme-powerlevel10k-git && cd zsh-theme-powerlevel10k-git makepkg -si && cd ..
 chsh -s /bin/zsh
