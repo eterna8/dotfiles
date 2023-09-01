7630ac6673d0f3d474425180d8633e00953480e4
eterna8 <eterna8@proton.me>
2023-09-01 21:59:13 +0300

    script fix

--- arch-install.sh
+++ arch-install.sh
@@ -1,8 +1,8 @@
 echo downoad needed packages started...
 sudo pacman -S polybar dolphin alacritty dunst rofi qt5ct kvantum  flameshot ttf-font-awesome ttf-cascadia-code zsh kcolorchooser
-yay -S i3-gaps-rounded-git
+git clone --depth 1 https://aur.archlinux.org/packages/i3-gaps-rounded-git && cd i3-gaps-rounded-git && makepkg -si && cd ..
+git clone --depth 1 https://aur.archlinux.org/packages/zsh-theme-powerlevel10k-git && cd zsh-theme-powerlevel10k-git makepkg -si && cd ..
 chsh -s /bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-yay -S zsh-theme-powerlevel10k-git polybar-spotify-module
 echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
-sh setup.sh 
+sh setup.sh
